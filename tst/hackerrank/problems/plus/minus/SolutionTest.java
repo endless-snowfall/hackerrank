66d876e752d3f4158eef6177a09243ca364d101f
endless.snowfall <endless.snowfall@gmail.com>
2015-11-22 19:10:00 -0500

    Adding PlusMinus Problem.

---
+++ tst/hackerrank/problems/plus/minus/SolutionTest.java
@@ -0,0 +1,28 @@
+package hackerrank.problems.plus.minus;
+
+import static hackerrank.problems.common.TestUtils.*;
+
+import org.junit.Test;
+
+import hackerrank.problems.common.TestBase;
+
+public class SolutionTest extends TestBase {
+
+    @Test
+    public void test1() {
+        System.setIn(mockInputStream("6 -4 3 -9 0 4 1"));
+        Solution.main(new String[] {});
+    }
+
+    @Test
+    public void test2() {
+        System.setIn(mockInputStream("5 -4 3 -9 4 1"));
+        Solution.main(new String[] {});
+    }
+
+    @Test
+    public void test3() {
+        System.setIn(mockInputStream("0"));
+        Solution.main(new String[] {});
+    }
+}
